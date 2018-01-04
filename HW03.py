8606db976d311a36c08c0ab934018d41985b1421
Rikako <15hatoyar@gmail.com>
2018-01-03 20:37:29 -0800

    .
    .

---
+++ HW03.py
@@ -0,0 +1,173 @@
+from operator import add, mul
+
+
+def square(x):
+    return x * x
+
+
+def triple(x):
+    return 3 * x
+
+
+def identity(x):
+    return x
+
+
+def increment(x):
+    return x + 1
+
+
+# Q1
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    if k % 10 == 7:
+        return True
+    elif k < 10:
+        return False
+    else:
+        return has_seven(k // 10)
+
+
+# Q2
+def summation(n, term):
+    """Return the sum of the first n terms in the sequence defined by term.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2 ** x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    """
+    assert n >= 1, "The number of terms in the sequence, 'n', should be equal to or greater than 1."
+    answer, k = 0, 1
+    while k < n + 1:
+        answer += term(k)
+        k += 1
+    return answer
+
+
+# Q3
+def accumulate(combiner, base, n, term):
+    """Return the result of combining the first n terms in a sequence and base.
+    The terms to be combined are term(1), term(2), ..., term(n).  combiner is a
+    two-argument commutative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)   # 2 * 1^2 * 2^2 * 3^2
+    72
+    """
+    answer, k = base, 1
+    while k < n + 1:
+        answer = combiner(answer, term(k))
+        k += 1
+    return answer
+
+
+def summation_using_accumulate(n, term):
+    """Returns the sum of term(1) + ... + term(n). The implementation
+    uses accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    """
+    return accumulate(add, 0, n, term)
+
+
+def product_using_accumulate(n, term):
+    """An implementation of product using accumulate.
+
+    >>> product_using_accumulate(4, square)
+    576
+    >>> product_using_accumulate(6, triple)
+    524880
+    """
+    return accumulate(mul, 1, n, term)
+
+
+# Q4
+def filtered_accumulate(combiner, base, pred, n, term):
+    """Return the result of combining the terms in a sequence of N terms
+    that satisfy the predicate PRED.  COMBINER is a two-argument function.
+    If v1, v2, ..., vk are the values in TERM(1), TERM(2), ..., TERM(N)
+    that satisfy PRED, then the result is
+         BASE COMBINER v1 COMBINER v2 ... COMBINER vk
+    (treating COMBINER as if it were a binary operator, like +). The
+    implementation uses accumulate.
+
+    >>> filtered_accumulate(add, 0, lambda x: True, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> filtered_accumulate(add, 11, lambda x: False, 5, identity) # 11
+    11
+    >>> filtered_accumulate(add, 0, odd, 5, identity)   # 0 + 1 + 3 + 5
+    9
+    >>> filtered_accumulate(mul, 1, greater_than_5, 5, square)  # 1 * 9 * 16 * 25
+    3600
+    """
+
+    def combine_if(x, y):
+        if pred(y):
+            return combiner(x, y)
+        else:
+            return x
+
+    return accumulate(combine_if, base, n, term)
+
+
+def odd(x):
+    return x % 2 == 1
+
+
+def greater_than_5(x):
+    return x > 5
+
+
+# Q5
+def make_repeater(f, n):
+    """Return the function that computes the nth application of f.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> make_repeater(square, 0)(5)
+    5
+    """
+
+    def function(m):
+        k = 1
+        while k < n + 1:
+            m = f(m)
+            k += 1
+        return m
+
+    return function
